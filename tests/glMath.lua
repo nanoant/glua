7fb80d663345999b4e5b3d99d3875981bdc4091d
Adam Strzelecki <ono@java.pl>
2012-02-03 23:26:37 +0100

    Simplified math benchmark.

--- tests/glMath.lua
+++ tests/glMath.lua
@@ -1,34 +1,18 @@
-local gl   = require 'gl'
+local gl   = require 'matrix'
 local math = require 'math'
 
-local ortho = gl.ortho(0, 640, 480, 0, -100, 100)
-local proj  = gl.perspective(0, 640, 480, 0, -100, 100)
+local w, h = 800, 600
+local projection = gl.perspective(60, w / h, .1, 1000)
 
-local m2 = gl.mat2(2,1,
-                   0,2)
-local m3 = gl.mat3(2,1,0,
-                   0,2,0,
-                   0,0,2)
+local model  = gl.identity
+local view   = gl.translate(0, 0, -24)
+local test   = gl.vec4(0, 0, 0, 0)
 
-local rot = gl.rotate(.2,.3,.4)
-
-print(rot.t); print()
-print(rot.inv); print()
-
-print(gl.identity * gl.vec3(1,2,3)); print()
-
-print(m2.inv.inv); print()
-print(m3.inv.inv); print()
-
-local m = gl.mat(2,1,0,0,
-                 0,2,0,0,
-                 0,0,2,0,
-                 0,0,0,1)
-print(m.inv.inv); print()
-
-local trans = gl.translate(0,0,-2)
-
-print(trans*gl.vec(1,1,1,1)); print()
-
--- print(gl.Perspective(60, 640 / 480, 0.2, 1000000).mat3.inv.t)
-print(trans.inv.t.mat3)
+local start  = os.clock()
+local modelView = view * model
+for i = 1, 400 do
+  test = modelView * projection
+  -- test = test + projection * model * view * test
+end
+print(string.format('took %f seconds', os.clock()-start))
+print(test)
